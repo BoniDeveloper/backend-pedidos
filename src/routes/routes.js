a84ece0618089a08d9efb96276c57bb5d242c076
BoniDeveloper <brunoboni03@gmail.com>
2022-05-13 16:35:56 -0300

    Backend com SelectAll e SelectDetail

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,7 @@ const ClienteController = require ('../controllers/clienteControllers.js');
 const router = express.Router();
  
 router.post('/cliente', ClienteController.Insert);
+router.get('/cliente', ClienteController.SelectAll);
+router.get('/cliente/:id', ClienteController.SelectDetail);
  
 module.exports = router;
